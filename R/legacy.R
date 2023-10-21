55705792d5dd49ff03069faa6ae16a4da40aa703
rickhelmus <r.helmus@uva.nl>
2023-10-21 22:01:25 +0200

    small fixes

--- R/legacy.R
+++ R/legacy.R
@@ -8,18 +8,18 @@
 #' [installation functions in this package][installing] can be used to manage \pkg{patRoon} installations.
 #'
 #' The `toggleLegacy()` function is used to enable or disable legacy installations temporarily.
-#' 
+#'
 #' The `removeLegacy()` function is used to undo legacy installations.
 #'
 #' @section Background: This section is purely informative, but may be of use when you want to manually manage legacy
 #'   installations.
-#'   
+#'
 #'   The legacy installation script typically results in the following changes:
-#'   
+#'
 #'   1. `~/patRoon-install`: a directory with external dependencies (`MetFrag`, `SIRIUS`, ...) and may contain \R packages from the `patRoonDeps` repository depending on user input during the installation.
 #'   2. `~/.Rprofile-patRoon.R`: An \R script that should be loaded during \R startup, and initializes the configuration needed to use the files (1).
 #'   3. Code in `~/.Rprofile` to actually load (2) on \R startup (only if it exists)
-#'   
+#'
 #'   When `toggleLegacy()` is used to disable legacy installations the function simply renames the initialization script
 #'   (2) so it won't be loaded on \R startup. The `removeLegacy()` function removes the files from (1-2) and optionally
 #'   the changes from (3).
@@ -69,16 +69,16 @@ inspectLegacyInstall <- function()
 toggleLegacy <- function(enable = NULL)
 {
     insp <- inspectLegacyInstall()
-    
+
     if (is.null(enable))
     {
         if (insp$RprofPatExists && insp$RprofPatDisExists)
             stop("Cannot detect if legacy installation should be enabled or disabled, please set the enable argument to TRUE/FALSE", call. = FALSE)
         enable <- insp$RprofPatDisExists
     }
-    
-    src <- dest < - NULL
-    
+
+    src <- dest <- NULL
+
     if (enable)
     {
         if (!insp$RprofPatDisExists)
@@ -91,14 +91,14 @@ toggleLegacy <- function(enable = NULL)
             stop("Cannot locate init script", call. = FALSE)
         src <- getLegacyInitScript(); dest <- getLegacyInitScript(dis = TRUE)
     }
-    
+
     printf("The init script will be %s by renaming %s to %s.\n", if (enable) "enabled" else "disabled", src, dest)
 
     if (askProceed() && !file.rename(src, dest))
         stop("Failed to rename file!", call. = FALSE)
-    
+
     printLegDone()
-    
+
     invisible(NULL)
 }
 
@@ -117,7 +117,7 @@ removeLegacy <- function(restoreRProfile = FALSE)
         rmPaths <- c(rmPaths, getLegacyInitScript(dis = TRUE))
     if (insp$dataDirExists)
         rmPaths <- c(rmPaths, getLegacyDataDir())
-    
+
     if (length(rmPaths) == 0 && (!restoreRProfile || !insp$RprofExists))
     {
         printf("Nothing needs to be done ...\n")
@@ -132,14 +132,14 @@ removeLegacy <- function(restoreRProfile = FALSE)
         else
         {
             nip <- normalizePath(getLegacyInitScript(), mustWork = FALSE, winslash = "/")
-            
+
             # text used by legacy installation script
             rpLines <- c("# Automatically added by install_patRoon script on ", # note partial, comment ends with date
                          sprintf("if (file.exists('%s'))", nip),
                          sprintf("    source('%s')", nip))
             pl <- readLines(npp)
             matches <- unlist(lapply(rpLines, grep, pl, fixed = TRUE))
-            
+
             # make sure that the sequence is found exactly once and in the right order
             if (length(matches) == length(rpLines) && !is.unsorted(matches))
             {
@@ -150,14 +150,17 @@ removeLegacy <- function(restoreRProfile = FALSE)
                 printf("No need to restore ~/.Rprofile\n")
         }
     }
-    
-    printf("The legacy installation will be removed by deleting the following files and directories: %s\n",
-           paste0(rmPaths, collapse = ", "))
-    
-    if (askProceed() && !all(unlink(rmPaths, recursive = TRUE)))
-        stop("Error while deleting files!", call. = FALSE)
-    
+
+    if (length(rmPaths) > 0)
+    {
+        printf("The legacy installation will be removed by deleting the following files and directories: %s\n",
+               paste0(rmPaths, collapse = ", "))
+
+        if (askProceed() && unlink(rmPaths, recursive = TRUE) != 0)
+            stop("Error while deleting files!", call. = FALSE)
+    }
+
     printLegDone()
-    
+
     invisible(NULL)
 }
